324639ed0d86498a7cf28b279f8a0b31afafdc3b
Dharanitharan P <susilar009@gmail.com>
2023-11-27 18:40:27 +0000

    commit

--- app/(main)/(routes)/workspace/[documentId]/components/CoverImage.tsx
+++ app/(main)/(routes)/workspace/[documentId]/components/CoverImage.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import Image from "next/image";
 import CoverImageModel from "./CoverImageModel";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface CoverImageProps {
   url?: string;
@@ -10,9 +14,20 @@ interface CoverImageProps {
 }
 
 export default function CoverImage({ url, preview }: CoverImageProps) {
+    const params = useParams();
+    const removeCoverImg = useMutation(api.documents.removeCoverImage);
+
+    const handleRemoveCoverImage = (
+        e:React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+        e.stopPropagation();
+
+        removeCoverImg({id: params.documentId as Id<"documents">})
+    }
+
   return (
     <div
-      className={`relative w-full h-[35vh] group/cover -z-10 ${
+      className={`relative w-full h-[35vh] group/cover ${
         !url && "h-[12vh]"
       } ${url && "bg-base-200"}`}
     >
@@ -21,13 +36,13 @@ export default function CoverImage({ url, preview }: CoverImageProps) {
           src={url}
           alt="cover/image"
           fill
-          className="object-cover object-left-top"
+          className="object-cover object-left-top -z-10"
         />
       )}
       {url && !preview && (
         <div    
             className="opacity-0 group-hover/cover:opacity-100 absolute bottom-5 right-20 flex 
-            items-center z-50 gap-x-2 h-fit w-fit"
+            items-center z-50 gap-x-2 h-fit w-fit transition-all delay-300"
         >
           <CoverImageModel>
             <button className="bg-white px-3 py-1 text-xs text-base-content rounded-sm hover:bg-base-300">
@@ -36,7 +51,7 @@ export default function CoverImage({ url, preview }: CoverImageProps) {
           </CoverImageModel>
           <button
             className="bg-white px-3 py-1 text-xs text-base-content rounded-sm hover:bg-base-300"
-            onClick={() => {}}
+            onClick={handleRemoveCoverImage}
           >
             Remove
           </button>
