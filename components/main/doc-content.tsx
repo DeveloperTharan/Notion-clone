2c7d1a4253132944d670b18cd134a6b6d5be05b6
DeveloperTharan <dharanitharanps@gmail.com>
2024-04-26 23:15:52 +0530

    🥗

--- components/main/doc-content.tsx
+++ components/main/doc-content.tsx
@@ -8,8 +8,6 @@ import { ToolBar } from "./toolbar";
 import { Document } from "@prisma/client";
 import { CoverImage } from "./cover-image";
 import { handleBody } from "@/actions/document";
-import { useSearchParams } from "next/navigation";
-import { useSession } from "next-auth/react";
 
 interface DocContentProps {
   document: Document | null;
@@ -30,33 +28,21 @@ export const DocContent = ({
     []
   );
 
-  const searchParams = useSearchParams();
-  const { data } = useSession();
-
-  const user = searchParams.get("user");
-  const isPublished = searchParams.get("isPublished");
-
   const handleOnChange = debounce((body: string) => {
     handleBody(document?.id!, body);
   }, 1000);
 
   return (
-    <>
-      {!isPublished && user == data?.user?.id ? (
-        <div className="pb-40 w-full">
-          <CoverImage url={document?.coverImage} preview={preview} />
-          <div className="max-w-xl lg:max-w-2xl xl:mxa-w-3xl 2xl:max-w-4xl mx-auto relative -mt-6 z-30 px-5">
-            <ToolBar initialData={document} preview={preview} />
-            <Editor
-              onChange={handleOnChange}
-              initialData={document?.body}
-              editable={editable}
-            />
-          </div>
-        </div>
-      ) : (
-        <div>Private document</div>
-      )}
-    </>
+    <div className="pb-40 w-full">
+      <CoverImage url={document?.coverImage} preview={preview} />
+      <div className="max-w-xl lg:max-w-2xl xl:mxa-w-3xl 2xl:max-w-4xl mx-auto relative -mt-6 z-30 px-5">
+        <ToolBar initialData={document} preview={preview} />
+        <Editor
+          onChange={handleOnChange}
+          initialData={document?.body}
+          editable={editable}
+        />
+      </div>
+    </div>
   );
 };
